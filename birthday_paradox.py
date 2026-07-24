79a511eb5c16c9efcfa34b709a02542148b9777a
Rober <boblawyer1@gmail.com>
2026-07-24 12:41:57 -0300

    Implement birthday generation logic in getBirthdays function

--- birthday_paradox.py
+++ birthday_paradox.py
@@ -1,4 +1,10 @@
 import datetime, random
 
 def getBirthdays(numberOfBirthdays):
-    pass
+    birthdays = []
+    for i in range(numberOfBirthdays):
+        startOfYear = datetime.date(2001, 1, 1)
+        randomNumberOfDays = datetime.timedelta(random.randint(0, 364))
+        birthday = startOfYear + randomNumberOfDays
+        birthdays.append(birthday)
+    return birthdays
