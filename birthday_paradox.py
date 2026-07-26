15d5dd18200bde83fbc7c0f3f11e28514cd1a2cd
Rober <boblawyer1@gmail.com>
2026-07-26 19:49:59 -0300

    Refactor birthday simulation logic and enhance output formatting

--- birthday_paradox.py
+++ birthday_paradox.py
@@ -18,3 +18,53 @@ def getMatch(birthdays):
             if birthdayA == birthdayB: 
                 return birthdayA # Return the matching birthday.
 
+print('''Birthday Paradox. The Birthday Paradox shows us that in a group of N people, 
+the odds that two of them have matching birthdays is surprisingly large. 
+This program does a Monte Carlo simulation (that is, repeated random simulations) to explore this concept.
+(It's not actually a paradox, it's just a surprising result.)''') 
+
+MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
+while True:
+    print('How many birthdays shall I generate? (max 100)')
+    response = input('> ')
+    if response.isdecimal() and (0 < int(response) <= 100):
+        numBdays = int(response)
+        break
+
+print()
+
+print('Here are', numBdays, 'birthdays:') 
+birthdays = getBirthdays(numBdays) 
+for i, birthday in enumerate(birthdays): 
+    if i != 0: 
+        print(', ', end='')
+    monthName = MONTHS[birthday.month - 1]
+    dateText = '{} {}'.format(monthName, birthday.day)
+    print(dateText, end='')
+    print()
+    print()
+
+    match = getMatch(birthdays)
+    print('In this simulation, ', end='')
+    if match != None:
+        monthName = MONTHS[match.month - 1]
+        dateText = '{} {}'.format(monthName, match.day)
+        print('multiple people have a birthday on ', dateText)
+    else:
+        print('there are no matching birthdays.')
+    print()
+
+    print('Generating ', numBdays, ' random birthdays 100.000 times...')
+    input('Press enter to begin...')
+
+    print('Let\'s run another 100.000 simulations.')
+    simMatch = 0
+    for i in range(100000):
+        if i % 10000 == 0:
+            print(i, 'simulations run...')
+    birthdays = getBirthdays(numBdays)
+    if getMatch(birthdays) != None:
+        simMatch += 1
+    print('100.000 simulations run.')
+    
