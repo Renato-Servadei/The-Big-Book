0165a586fb2689d79ef816fda32a6793feeed394
Rober <boblawyer1@gmail.com>
2026-07-28 20:31:53 -0300

    Implement main game loop and player actions for Blackjack game

---
+++ black_jack.py
@@ -0,0 +1,88 @@
+import random, sys
+
+HEARTS = chr(9829)
+DIAMONDS = chr(9830)
+SPADES = chr(9824)
+CLUBS = chr(9827)
+
+BACKSIDE = 'backside'
+
+def main():
+    print('''
+    Rules:
+    Try to get as close to 21 without going over. 
+    Kings, Queens, and Jacks are worth 10 points. 
+    Aces are worth 1 or 11 points. 
+    Cards 2 through 10 are worth their face value. 
+    (H)it to take another card. 
+    (S)tand to stop taking cards. 
+    On your first play, you can (D)ouble down to increase your bet 
+    but must hit exactly one more time before standing. 
+    In case of a tie, the bet is returned to the player. 
+    The dealer stops hitting at 17.''')
+
+    money = 5000
+# Main game loop.
+    while True:  
+# Check if the player has run out of money:
+        if money <= 0:
+            print('You\'re broke!')
+            print("Good thing you weren't playing with real money.")
+            print('Thanks for playing!')
+            sys.exit()
+
+#let the player enter the bet for this round.
+        print('Money: ', money)
+        bet = getBet(money)
+
+# Give the dealer and player two cards from the deck each:
+        deck = getDeck()
+        dealerHand = [deck.pop(), deck.pop()]
+        playerHand = [deck.pop(), deck.pop()]
+
+#Handle player actions
+        print('Bet: ', bet)
+# Keep looping until player stands or busts.
+        while True:
+            displayHands(dealerHand, playerHand, False)
+            print()
+#Check if the player has bust
+            if getHandValue(playerHand) > 21:
+                break
+# Get the player's move, either H, S, or D:
+            move = getMove(playerHand, money - bet)
+
+#Handle the player actions
+    # Player is doubling down, they can increase their bet:
+            if move == 'D':
+                additionalBet = getBet(min(bet, (money - bet)))
+                bet += additionalBet
+                print('Bet increased to {}'.format(bet))
+                print('Bet: ', bet)
+    # Hit/doubling down takes another card.
+            if move in ('H', 'D'):
+                newCard = deck.pop()
+                rank, suit = newCard
+                print('You drew a {} of {}.'.format(rank, suit))
+                playerHand.append(newCard)
+#The player has busted
+                if getHandValue(playerHand) > 21:
+                    continue
+# Stand/doubling down stops the player's turn.
+            if move in ('S', 'D'):
+                break
+
+def getBet(money):
+    pass
+
+def getDeck():
+    pass
+
+def getHandValue():
+    pass
+
+def displayHands():
+    pass
+
+def getMove():
+    pass
