c3809c7de22aefda260623fa4ce851e62aa784fc
Rober <boblawyer1@gmail.com>
2026-08-01 18:48:23 -0300

    Implement Hand class with methods to add cards and calculate hand value

--- black_jack3.0.py
+++ black_jack3.0.py
@@ -2,3 +2,28 @@ class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+
+    def add(self, card):
+        self.cards.append(card)
+
+    def value(self):
+        value, aces = 0, 0
+
+        for c in self.cards:
+            if c.rank == 'A':
+                aces += 1
+            elif c.rank in ('K', 'Q', 'J'):
+                value += 10
+            else:
+                value += int(c.rank)
+
+        value += aces
+        for _ in range(aces):
+            if value + 10 <= 21:
+                value += 10
+
+        return value
